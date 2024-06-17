63c58df5fcb572e8e32f40e448b2fe8cd210ff12
colinl <clanlaw@gmail.com>
2024-06-17 11:52:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# A ui node for Node-RED Dashboard 2.0
+
+A ui node for the flowfuse [Node-RED](https://nodered.org) Dashboard 2.0
+
+## Install
+
+The usual method of installing is to use Manage Palette in the node red editor and search for _package_name_ and install it.
+
+Using `npm` directly, `cd` into your node red user directory (usually .node-red in your home folder) and from there run
+```
+npm install _package_name_
+```
+
+## Inputs
+
+## Configuration
+
+* **Name** - The name of the node.
+* **Group** - The display group in which to show the widget.
+* **Size** - The size of the widget on the dashboard.
+
+
+* **Class** - A CSS Class that will be applied to the wodget to allow override of display element style.
+
+## Dynamic Properties
+
+
+## CSS overrides
+
+Display elements my be overriden in the normal way using a ui-template node.  In particular:
+
---
+++ ui/components/_NodeName_.vue
@@ -0,0 +1,92 @@
+<!-- A dashboard 2 widget
+
+-->
+<template>
+    <!-- Component must be wrapped in a block so props such as className and style can be passed in from parent -->
+    <div className="_node_name_-wrapper" :class="class">
+
+    </div>
+</template>
+
+<script>
+//import { markRaw } from 'vue'
+import { mapState } from 'vuex'
+
+export default {
+    name: '_NodeName_',
+    inject: ['$socket'],
+    props: {
+        /* do not remove entries from this - Dashboard's Layout Manager's will pass this data to your component */
+        id: { type: String, required: true },
+        props: { type: Object, default: () => ({}) },
+        state: { type: Object, default: () => ({ enabled: false, visible: false }) }
+    },
+    setup (props) {
+        //console.info('_nodeName_ setup with:', props)
+        //console.debug('Vue function loaded correctly', markRaw)
+    },
+    data () {
+        return {
+
+            class: "",
+        }
+    },
+    computed: {
+        ...mapState('data', ['messages']),
+    },
+    mounted () {
+        this.$socket.on('widget-load:' + this.id, (msg) => {
+            // load the latest message from the Node-RED datastore when this widget is loaded
+            // storing it in our vuex store so that we have it saved as we navigate around
+            console.log(`On widget-load ${JSON.stringify(msg)}`)
+            this.processMsg(msg)     // pick up message values
+            /*
+            havent worked out how to use this yet
+            this.$store.commit('data/bind', {
+                widgetId: this.id,
+                msg
+            })
+            */
+        })
+        this.$socket.on('msg-input:' + this.id, (msg) => {
+            console.log(`Message received: ${JSON.stringify(msg)}`)
+            // new message received
+            this.processMsg(msg)
+
+            // store the latest message in our client-side vuex store when we receive a new message
+            /*
+            this.$store.commit('data/bind', {
+                widgetId: this.id,
+                msg
+            })
+            */
+        })
+
+        //console.log(`mounted, props: ${JSON.stringify(this.props)}`)
+        // pickup node properties to local data
+        this.pickupProperties()
+        // tell Node-RED that we're loading a new instance of this widget
+        this.$socket.emit('widget-load', this.id)
+    },
+    unmounted () {
+        /* Make sure, any events you subscribe to on SocketIO are unsubscribed to here */
+        this.$socket?.off('widget-load:' + this.id)
+        this.$socket?.off('msg-input:' + this.id)
+    },
+    methods: {
+        pickupProperties: function() {
+            // pickup node properties from this.props and merge with base properties
+            const props = this.props
+            // ...
+        },
+        processMsg: function(msg) {
+            // ...
+        },
+    },
+}
+</script>
+
+<style scoped>
+    /* CSS is auto scoped, but using named classes is still recommended */
+    @import "../stylesheets/_node_name_.css";
+</style>
